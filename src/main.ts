5957be08850a9f017dadba24225e0db4a9536c28
“Keaton <kshawhan@ucsc.edu>
2024-11-05 13:47:28 -0800

    small change for L16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+const button = document.createElement("button");
+
+button.innerText = "Click me";
+
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+document.body.appendChild(button);
